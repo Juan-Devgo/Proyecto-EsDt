3bd5f2ee339238c6b756d58fb47382fcd65d4e13
Juan-Devgo <juand.garcian@uqvirtual.edu.co>
2025-03-29 09:35:44 -0500

    1: Creación del proyecto

---
+++ src/main/java/co/edu/uniquindio/proyectoesdt/ProyectoEsdtApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.proyectoesdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEsdtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoEsdtApplication.class, args);
+    }
+
+}
